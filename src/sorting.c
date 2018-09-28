af0a74cf27c64bf64997e85db1a4a08a03fd03f3
Andreas Blin <ablin@student.42.fr>
2018-09-29 00:25:51 +0200

    pausing

--- src/sorting.c
+++ src/sorting.c
@@ -20,7 +20,6 @@ int			pick_pivot_med(t_ctrl *ctrl, t_node *stka)
 {
 	int		arr[ctrl->size_a];
 	int		i;
-	int		nb;
 
 	i = 1;
 	arr[0] = ps_getminnb(ctrl, stka, ctrl->size_a, 1);
@@ -29,7 +28,6 @@ int			pick_pivot_med(t_ctrl *ctrl, t_node *stka)
 		arr[i] = ps_next_nb_a(ctrl, stka, arr[i - 1], 1);
 		i++;
 	}
-	i = 0;
 	return (arr[ctrl->size_a / 2]);
 }
 
@@ -64,91 +62,14 @@ void		ps_quicksort(t_ctrl **ctrl, t_node **stka, t_node **stkb, int pivot)
 		ps_cycle_move(ctrl, stka, stkb, RRA);
 	while ((*stka)->nb > pivot && ps_is_sorted(*stka) != 1)
 		ps_cycle_move(ctrl, stka, stkb, RA);
-	//while ((*ctrl)->size_a > 1 && (*stka)->nb <= pivot
-	//&& ps_is_sorted(*stka) != 1)// && (*ctrl)->size_a > (*ctrl)->size_b)
-	//{
+	while ((*ctrl)->size_a > 1 && (*stka)->nb <= pivot
+	&& ps_is_sorted(*stka) != 1)// && (*ctrl)->size_a > (*ctrl)->size_b)
+	{
 		if ((*ctrl)->size_a > 1
 		&& ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
 			ps_cycle_move(ctrl, stka, stkb, SA);
 		if (ps_is_sorted(*stka) != 1)
 			ps_cycle_move(ctrl, stka, stkb, PB);
-	//}
-}
-
-int			ps_getmode(t_node *stka, int pivot)
-{
-	int		above;
-	int		under;
-	t_node	*tmp;
-
-	above = 0;
-	under = 0;
-	tmp = stka;
-	while (tmp->nb != pivot)
-	{
-		if (tmp->nb > pivot)
-			above++;
-		else if (tmp->nb < pivot)
-			under++;
-		tmp = tmp->next;
-	}
-	if (above > under || above == 0)
-		return (under * -1);
-	return (above);
-}
-
-void		ps_selfsort(t_ctrl **ctrl, t_node **stka, t_node **stkb, int pivot)
-{
-	int		mode;
-
-	mode = ps_getmode(*stka, pivot);
-	ft_printf("[%d][%d]\n", mode, pivot);
-	while (mode > 0)
-	{
-		if (ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
-			ps_cycle_move(ctrl, stka, stkb, SA);
-		else if ((*stka)->nb > pivot && (*stka)->nb != pivot)
-		{
-			ps_cycle_move(ctrl, stka, stkb, PB);
-			mode--;
-		}
-		else
-			ps_cycle_move(ctrl, stka, stkb, RA);
-	}
-	while (mode < 0 && (*stka)->nb != pivot)
-	{
-		if (ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
-			ps_cycle_move(ctrl, stka, stkb, SA);
-		else if ((*stka)->nb < pivot)
-		{
-			ps_cycle_move(ctrl, stka, stkb, PB);
-			mode++;
-		}
-		else
-			ps_cycle_move(ctrl, stka, stkb, RA);
-	}
-	if (mode == 0 && (*stka)->nb == pivot)
-		ps_cycle_move(ctrl, stka, stkb, RA);
-}
-
-void		ps_mergesort(t_ctrl **ctrl, t_node **stka, t_node **stkb)
-{
-	int		i;
-	//int		start;
-	
-	i = 0;
-	while (ps_is_sorted(*stka) != 1)//while stka nb != start
-	{
-	//	ft_printf("[%d]\n", (*ctrl)->size_a);
-		if ((*stka)->nb > (*stka)->next->nb)
-			ps_cycle_move(ctrl, stka, stkb, PB);
-		else
-		{
-		//	if (i == 0)
-		//		start = (*stka)->nb;
-			i++;
-			ps_cycle_move(ctrl, stka, stkb, RA);
-		}
 	}
 }
 
@@ -158,19 +79,13 @@ void		ps_sort(t_ctrl **ctrl, t_node **stka)
 	t_node	*stkb;
 
 	stkb = NULL;
-	//ft_printf("{%d}{%d}{%d}\n", pick_pivot_moy(*ctrl, *stka), pick_pivot_mid(*ctrl, *stka), pick_pivot_med(*ctrl, *stka));
-//	while (ps_is_sorted(*stka) != 1)// && (*ctrl)->size_a > (*ctrl)->size_b)
-//		ps_quicksort(ctrl, stka, &stkb, pick_pivot_med(*ctrl, *stka));
-	//ft_putstr("QUICKSORT ENDED NOW\n");
-while (ps_is_sorted(*stka) != 1)
-	ps_selfsort(ctrl, stka, &stkb, pick_pivot_med(*ctrl, *stka));
-//	ps_mergesort(ctrl, stka, &stkb);
-//	ft_putstr("MERGESORT ENDED NOW\n");
-//	while ((*ctrl)->size_b > 0)
-//		ps_selectsort(ctrl, stka, &stkb);
-//	ft_putstr("SELECTSORT ENDED NOW\n");
-//	while (ps_getminnb(*ctrl, *stka, (*ctrl)->size_a, 0) != 0)
-//		ps_cycle_move(ctrl, stka, &stkb, RRA);
+	while (ps_is_sorted(*stka) != 1)// && (*ctrl)->size_a > (*ctrl)->size_b)
+		ps_quicksort(ctrl, stka, &stkb, pick_pivot_med(*ctrl, *stka));
+	while ((*ctrl)->size_b > 0)
+		ps_selectsort(ctrl, stka, &stkb);
+	ft_putstr("SELECTSORT ENDED NOW\n");
+	while (ps_getminnb(*ctrl, *stka, (*ctrl)->size_a, 0) != 0)
+		ps_cycle_move(ctrl, stka, &stkb, RRA);
 	ft_putstr((*ctrl)->str);
 }
 /*void		ps_selectsort(t_ctrl **ctrl, t_node **stka, t_node **stkb)
@@ -201,4 +116,82 @@ while (ps_is_sorted(*stka) != 1)
 		ps_cycle_move(ctrl, stka, stkb,
 		ps_up_or_down(*ctrl, *stka, next_a, 0));
 	ps_cycle_move(ctrl, stka, stkb, PA);
+}*/
+/*
+int			ps_getmode(t_node *stka, int pivot)
+{
+	int		above;
+	int		under;
+	t_node	*tmp;
+
+	above = 0;
+	under = 0;
+	tmp = stka;
+	while (tmp->nb != pivot)
+	{
+		if (tmp->nb > pivot)
+			above++;
+		else if (tmp->nb < pivot)
+			under++;
+		tmp = tmp->next;
+	}
+	if (above > under || above == 0)
+		return (under * -1);
+	return (above);
+}
+void		ps_selfsort(t_ctrl **ctrl, t_node **stka, t_node **stkb, int pivot)
+{
+	int		mode;
+
+	mode = ps_getmode(*stka, pivot);
+	ft_printf("[%d][%d]\n", mode, pivot);
+	while (mode > 0 && (*stka)->nb != pivot)
+	{
+		if (ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
+			ps_cycle_move(ctrl, stka, stkb, SA);
+		else if ((*stka)->nb > pivot)
+		{
+			ps_cycle_move(ctrl, stka, stkb, PB);
+			mode--;
+		}
+		else
+			ps_cycle_move(ctrl, stka, stkb, RA);
+	}
+	while (mode < 0 && (*stka)->nb != pivot)
+	{
+		if (ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
+			ps_cycle_move(ctrl, stka, stkb, SA);
+		else if ((*stka)->nb < pivot)
+		{
+			ps_cycle_move(ctrl, stka, stkb, PB);
+			mode++;
+		}
+		else
+			ps_cycle_move(ctrl, stka, stkb, RA);
+	}
+	if (ps_next_nb_a(*ctrl, *stka, (*stka)->next->nb, 0) == 0)
+			ps_cycle_move(ctrl, stka, stkb, SA);
+	else if (mode == 0 && (*stka)->nb == pivot)
+		ps_cycle_move(ctrl, stka, stkb, RA);
+}
+
+void		ps_mergesort(t_ctrl **ctrl, t_node **stka, t_node **stkb)
+{
+	int		i;
+	//int		start;
+	
+	i = 0;
+	while (ps_is_sorted(*stka) != 1)//while stka nb != start
+	{
+	//	ft_printf("[%d]\n", (*ctrl)->size_a);
+		if ((*stka)->nb > (*stka)->next->nb)
+			ps_cycle_move(ctrl, stka, stkb, PB);
+		else
+		{
+		//	if (i == 0)
+		//		start = (*stka)->nb;
+			i++;
+			ps_cycle_move(ctrl, stka, stkb, RA);
+		}
+	}
 }*/
--- src/tools.c
+++ src/tools.c
@@ -26,74 +26,18 @@ int			ps_up_or_down(t_ctrl *ctrl, t_node *stka, int nb, int stack)
 		return (RRB);
 	return (RRA);
 }
-/*
-int		ps_find_suite(t_ctrl *ctrl, t_node *stka, int indexnb, int mode)
+
+int			ps_next_snb(t_ctrl *ctrl, t_node *stka)
 {
-	t_node	*tmp;
 	int		i;
 
-	tmp = stka;
 	i = 0;
-	if (mode == 1)
-	{
-		while (tmp->nb != indexnb && tmp->next != ctrl->head_a)
-			tmp = tmp->next;
-		if (tmp->next != ctrl->head_a)
-			tmp = tmp->next->next;
-	}
-	while (tmp->next != ctrl->head_a)
-	{
-		if (tmp->nb != ps_getmaxnb(ctrl, stka, 1) && tmp->nb > tmp->next->nb)
-			return (tmp->nb);
-		tmp = tmp->next;
-	}
-	return (0);
-}*/
-/*
-int		ps_valid_suite(t_ctrl *ctrl, t_node *stka)
-{
-	t_node	*tmp;
-	int		i;
-	int		j;
-
-	tmp = stka;
-	i = 0;
-	j = 0;
-	while (j < ctrl->size_a - 1)
-	{
-		if (tmp->nb != ps_getmaxnb(ctrl, stka, 1) && tmp->nb > tmp->next->nb)
-		{
-			i++;
-			j++;
-			tmp = tmp->next;
-		}
-		j++;
-		tmp = tmp->next;
-	}
-	return (i);
-}*/
-
-int			ps_lowest_suite_nb(t_ctrl *ctrl, t_node *stka, int st, int st2)
-{
-	int		i;
-	int		low;
-	int		low2;
-	t_node	*tmp;
-
-	i = 1;
-	low = 0;
-	low2 = 0;
-	tmp = stka;
 	while (i < ctrl->size_a)
 	{
-		if (tmp->nb == st)
-			low = tmp->next->nb;
-		else if (tmp->nb == st2)
-			low2 = tmp->next->nb;
-		tmp = tmp->next;
+		if (stka->nb > stka->next->nb)
+			return (stka->nb);
 		i++;
+		stka = stka->next;
 	}
-	if (low > low2)
-		return (st2);
-	return (st);
+	return (-1);
 }
